82ce489ca05e61f7c91507200a187d33e410603d
Sakshamworks <sakshamsaksham312@gmail.com>
2026-03-20 16:27:22 +0000

    To find Area Of Rectangle

---
+++ 01_problem.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter length of rectangle : ");
+    scanf("%d",&a);
+    printf("Enter bridth of rectangle : ");
+    scanf("%d",&b);
+    printf("area of rectangle: %d",a*b);
+    return(0);
+}
