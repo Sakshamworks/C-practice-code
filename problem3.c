20c8a5c9044e36dcb68f5a131a5bd68acfaa5209
Sakshamworks <sakshamsaksham312@gmail.com>
2026-03-21 05:09:58 +0000

    to find area of cylinder

---
+++ problem3.c
@@ -0,0 +1,15 @@
+//area of cylinder
+#include<stdio.h>
+int main(){
+    float r;
+    printf("the radius of cylinder : ");
+    scanf("%f",&r);
+    float h;
+    printf("the hight of cylinder : ");
+    scanf("%f",&h);
+    printf("the area of cylinder is %.2f, :  ",2*3.14*r*h+2*3.14*r*r);
+    return(0);
+
+
+    
+}
