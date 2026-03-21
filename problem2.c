5a8f9d0cf953599b78253e60428e02e6c6c5c574
Sakshamworks <sakshamsaksham312@gmail.com>
2026-03-21 03:59:23 +0000

    to find area of circle

---
+++ problem2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float r;
+    printf("the radius of circle : ");
+    scanf("%f",&r);
+    printf("thr area of circle is %.2f :  ",3.14*r*r);
+    return(0);
+
+
+    
+}
