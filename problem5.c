1edbad3cfd2c743ac14c87cf25e5a028e0237fa5
Sakshamworks <sakshamsaksham312@gmail.com>
2026-03-22 16:01:05 +0000

    convert

---
+++ problem5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float a;
+    printf("enter how much centigrade is it : ");
+    scanf("%f",&a);
+    printf("after converting in fahrenheit %.2f:",(a*1.8)+32);
+    return(0);
+
+
+}
