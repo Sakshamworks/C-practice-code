d1e42a225b67bfaee5f99f31c4e29239af5f8103
Sakshamworks <sakshamsaksham312@gmail.com>
2026-04-15 05:14:58 +0100

    sum of two flot number

---
+++ practice.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float a,b;
+    printf("enter the value of a:");
+    scanf("%f",&a);
+    printf("enter the value of b:");
+    scanf("%f",&b);
+    printf("the sum of both number is :%.2f ",a+b);
+    return 0;
+
+}
