6ffe7022948690c29e2d845ef273e454f5639b71
Sakshamworks <sakshamsaksham312@gmail.com>
2026-04-17 17:09:18 +0100

    to find area of circle

---
+++ practice3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//for finding area of square//
+int main(){
+    float pi=3.14;
+    int r;
+    printf("the radius is ");
+    scanf("%d",&r);
+    printf("the radius of circle is %f : ",pi*r*r);
+    return (0);
+
+
+}
