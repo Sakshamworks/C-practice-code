151ac6a4c40ce2763b6f640dbaadc4e2bc2cab83
Sakshamworks <sakshamsaksham312@gmail.com>
2026-04-22 18:10:56 +0100

    dollar to INR

---
+++ problem15.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+float main()
+{
+    float a;
+    printf("enter how much dollar covert in rupees :");
+    scanf("%f", &a);
+    printf("after converting doller in rupees %.2f\n : ", a * 92.60);
+    return 0;
+}
