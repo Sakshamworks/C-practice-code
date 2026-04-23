6a6e956c907f978e38bcac651cc7485975c18339
Sakshamworks <sakshamsaksham312@gmail.com>
2026-04-23 18:34:15 +0100

    use of ternary operator

---
+++ problem18.c
@@ -0,0 +1,11 @@
+//uses of ternary operator
+#include<stdio.h>
+    int main(){
+        int a;
+        printf("enter a number :");
+        scanf("%d",&a);
+
+        a>=18? printf("adult\nit able for vote"):printf("not adult");
+        return 0;
+
+    }
