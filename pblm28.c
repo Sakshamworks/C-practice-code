2289f67c8d97aeb96e608b2e7723a13a3d3be7dd
Sakshamworks <sakshamsaksham312@gmail.com>
2026-04-29 12:59:10 +0100

    enter the number which table you want

---
+++ pblm28.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number which table u want :  ");
+    scanf("%d", &a);
+    for (int i = 1; i <= 10; i++)
+    {
+
+        printf("%d\n", a * i);
+    }
+    return 0;
+}
