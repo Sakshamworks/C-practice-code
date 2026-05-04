8ffc4f6522c0468fb43d7c8dca46b9ef19af15b5
Sakshamworks <sakshamsaksham312@gmail.com>
2026-05-04 16:55:54 +0530

    use of while loop

---
+++ pblm30.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+    /*int a,b,c;
+    printf("enter the 1st odd number : ");
+    scanf("%d",&b);
+    printf("enter the 2nd odd number to find odd number between two number : ");
+    scanf("%d",&c);
+    for(int i=b;i<=c;i++
+    ){
+        printf("%d\n",++i);
+    }*/
+   int i=1;
+   while(i<=10){
+   printf("%d\n",i);
+    i++;
+   }
+   
+    return 0;
+}
