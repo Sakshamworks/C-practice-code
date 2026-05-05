aebbffa25e8a64572dac79b77c5223d73cbc8fb7
Sakshamworks <sakshamsaksham312@gmail.com>
2026-05-05 15:44:29 +0530

    adding of n number using loop

---
+++ pblm34.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int m , n=0;
+    printf("enter the number : ");
+    scanf("%d",&m);
+    for(int a=1;a<=m;a++){
+       n=n+a ;
+    }
+     printf("%d",n);
+     return 0;
+}
+
+
+
+
+
+
+
