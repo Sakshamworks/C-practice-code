f476a5f4b8c5eea3c2c3ad4279062e3c635a5537
Sakshamworks <sakshamsaksham312@gmail.com>
2026-05-10 13:51:44 +0530

    use of function

---
+++ pblm43.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void helloworld();//function declaration OR prototype
+int main()
+{
+    helloworld();//function call
+}
+void helloworld()
+{
+    printf("hello");//function define
+}
